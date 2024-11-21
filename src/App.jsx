1c91d2bbe079d7c5b15c1636c1876c1592b761aa
sifatul-islam-bd <mdsifatulislam2357@gmail.com>
2024-11-21 21:48:19 +0600

    configer

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div className="bg-white_2_F5F5F5 font-Inter">App</div>;
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        white: "var(--white)",
+        white_2_F5F5F5: "var(--white_2_F5F5F5)",
+        black: "var(--black)",
+        black_2_363738: "var(--black_2_363738)",
+        white_3_FEFAF1: "var(--white_3_FEFAF1)",
+        bg_white_fff: "var(--bg_white_fff)",
+        text_fafafa: "var(--text_fafafa)",
+        text_2_7D8184: "var(--text_2_7D8184)",
+        text_black_000: "var(--text_black_000)",
+        button_black_000: "var(--button_black_000)",
+        red_DB4444: "var(--red_DB4444)",
+        button_green_00FF66: "var(--button_green_00FF66)",
+        button_red_2_DB4444: "var(--button_red_2_DB4444)",
+        rehover_pink_E07575d_DB4444: "var(--hover_pink_E07575)",
+        hover_2_A0BCE0: "var(--hover_2_A0BCE0)",
+      },
+    },
+    fontFamily: {
+      Poppins: "var(--Poppins)",
+      Inter: "var(--Inter)",
+    },
+    container: {
+      padding: {
+        // DEFAULT: "1rem",
+        // sm: "2rem",
+        // lg: "4rem",
+        // xl: "5rem",
+        // "2xl": "6rem",
+      },
+    },
+  },
+  plugins: [],
+};
